d5dbef74a98c3e72422212b381cd782d22cb7857
Daniel Londoño Henao <daniel.esloh@gmail.com>
2023-03-05 21:08:21 -0500

    Heroku fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,6 +82,8 @@ Rails.application.configure do
   # require "syslog/logger"
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new "app-name")
 
+  rubyconfig.force_ssl = true
+
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
@@ -91,4 +93,3 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
-rubyconfig.force_ssl = true
