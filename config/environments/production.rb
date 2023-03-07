3766de28f98a0597ca178716fd9bde83f5742e5b
Daniel Londoño Henao <daniel.esloh@gmail.com>
2023-03-07 17:59:15 -0500

    A

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,7 +82,7 @@ Rails.application.configure do
   # require "syslog/logger"
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new "app-name")
 
-  rubyconfig.force_ssl = true
+  config.force_ssl = true
 
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
